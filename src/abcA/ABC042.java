633f91ab4450a756578e1c0ca249f95040851f6e
Fuka Nishikawa <fuka.nishikawa@gmail.com>
2022-01-26 17:01:29 +0100

    Solved ABC042-A

--- src/abcA/ABC042.java
+++ src/abcA/ABC042.java
@@ -10,6 +10,23 @@ public class ABC042 {
         int b = scanner.nextInt();
         int c = scanner.nextInt();
 
+        String abc = "" + a + b + c;
 
+        int count5 = 0;
+        int count7 = 0;
+
+        for (int i = 0; i < abc.length(); i++) {
+            if (abc.charAt(i) == '5') {
+                count5++;
+            }
+            else if (abc.charAt(i) == '7') {
+                count7++;
+            }
+        }
+
+        if (count5 == 2 && count7 == 1) {
+            System.out.println("YES");
+        }
+        else System.out.println("NO");
     }
 }
